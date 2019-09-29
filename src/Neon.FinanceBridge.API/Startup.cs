a8dab8d62a58446b24ca5fc29d250449cc323fa3
Waqas Idrees <waqasidrees@hotmail.com>
2019-09-29 14:27:15 +0500

    UI Project is added and swagger is configured in api project.

--- src/Neon.FinanceBridge.API/Startup.cs
+++ src/Neon.FinanceBridge.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Neon.FinanceBridge.API
 {
@@ -26,6 +27,25 @@ namespace Neon.FinanceBridge.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Neon API",
+                    Description = "Finanace Bridge",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Waqas Idrees",
+                        Email = string.Empty
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +55,15 @@ namespace Neon.FinanceBridge.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
