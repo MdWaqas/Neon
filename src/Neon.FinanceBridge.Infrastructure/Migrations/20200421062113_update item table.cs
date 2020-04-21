72c02456a25e7c10524419b116af7267f64e919d
M Tahir Latif <mtahirlatifkhan@gmail.com>
2020-04-21 11:29:23 +0500

    add migration to resolve issue

---
+++ src/Neon.FinanceBridge.Infrastructure/Migrations/20200421062113_update item table.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Neon.FinanceBridge.Infrastructure.Migrations
+{
+    public partial class updateitemtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinimumQuantity",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AlertQuantity",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlertQuantity",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumQuantity",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
